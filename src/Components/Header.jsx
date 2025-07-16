fc4726fef0d4fbd6cf792bbba5348f62218b4bb4
Ahnaf Tahmid <ahnaf.ent@gmail.com>
2025-07-16 15:39:21 +0600

    responsive

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,37 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from '../Components/Container'
 import logo from '../assets/Logo.png'
 import { FaBars } from "react-icons/fa";
 import { RxCross1 } from "react-icons/rx";
 
-
-
 const Header = () => {
+  let [menu, setMenu] = useState(false);
+
   return (
     <header className='py-3'>
- <Container>
-    <div className='flex items-center'>
-        <div className='w-2/12'>
+      <Container>
+        <div className='lg:flex items-center'>
+          <div className='w-2/12'>
             <img src={logo} alt="" />
-        </div>
-        <div className='w-10/12'>
-            <ul className='flex gap-6 justify-center '>
-                <li className='font-dm text-[16px] text-[#767676] hover:text-[#262626] duration-300 ease-in-out'>Home</li>
-                <li className='font-dm text-[16px] text-[#767676] hover:text-[#262626] duration-300 ease-in-out'>Shop</li>
-                <li className='font-dm text-[16px] text-[#767676] hover:text-[#262626] duration-300 ease-in-out'>About</li>
-                <li className='font-dm text-[16px] text-[#767676] hover:text-[#262626] duration-300 ease-in-out'>Contact</li>
-                <li className='font-dm text-[16px] text-[#767676] hover:text-[#262626] duration-300 ease-in-out'>Journal</li>
+          </div>
+          <div className='w-full  lg:w-10/12'>
+            <ul className={`lg:flex gap-6 justify-center duration-300 ease-in-out  text-center
+              ${menu == true ? "bg-[#262626] h-[0px] opacity-0 overflow-hidden" 
+              : "bg-[silver] h-[200px] opacity-100"} 
+              lg:bg-transparent lg:h-auto lg:opacity-100 lg:overflow-visible`}>
+              <li className='font-dm text-[16px] text-[#767676] hover:text-[#262626] duration-300 ease-in-out'>Home</li>
+              <li className='font-dm text-[16px] text-[#767676] hover:text-[#262626] duration-300 ease-in-out'>Shop</li>
+              <li className='font-dm text-[16px] text-[#767676] hover:text-[#262626] duration-300 ease-in-out'>About</li>
+              <li className='font-dm text-[16px] text-[#767676] hover:text-[#262626] duration-300 ease-in-out'>Contact</li>
+              <li className='font-dm text-[16px] text-[#767676] hover:text-[#262626] duration-300 ease-in-out'>Journal</li>
             </ul>
+          </div>
+          <div className='lg:hidden absolute top-[10px] right-2' onClick={() => setMenu(!menu)}>
+            {menu ? <RxCross1 /> : <FaBars />}
+          </div>
         </div>
-        <div>
-        <FaBars className='lg:hidden'/>
-        <RxCross1 className='lg:hidden'/>
-        </div>
-    </div>
-</Container>
+      </Container>
     </header>
-   
   )
 }
 
-export default Header
+export default Header;
